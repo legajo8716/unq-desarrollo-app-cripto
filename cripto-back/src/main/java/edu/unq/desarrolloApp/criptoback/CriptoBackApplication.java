fe9d19c2e348eecab54a10892ac1ddb025aadd86
legajo8716 <nelgonzalez88@gmail.com>
2021-09-15 09:58:53 -0300

    estructura proyecto

---
+++ cripto-back/src/main/java/edu/unq/desarrolloApp/criptoback/CriptoBackApplication.java
@@ -0,0 +1,13 @@
+package edu.unq.desarrolloApp.criptoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoBackApplication.class, args);
+	}
+
+}
---
+++ cripto-back/src/test/java/edu/unq/desarrolloApp/criptoback/CriptoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unq.desarrolloApp.criptoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
