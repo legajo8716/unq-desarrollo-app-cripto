b8bec8e6bdd34f9cb163481ef282cf9f0d99e177
legajo8716 <nelgonzalez88@gmail.com>
2021-09-17 14:03:59 -0300

    avanzando con el front

--- cripto-front/src/index.js
+++ cripto-front/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './views/Login';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+global.jQuery = require('jquery');
+require('bootstrap');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
