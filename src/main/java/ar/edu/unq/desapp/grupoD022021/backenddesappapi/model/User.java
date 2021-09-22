afda7beb487cae2ffd1aa7dc11dcb3566e961370
PabloD97 <pablo97758@gmail.com>
2021-09-22 20:42:36 -0300

    feat: User model
    - Se agrego el constructor de la clase

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/model/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "persons")
-public class Person {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,7 +29,7 @@ public class Person {
     @Column(name = "number_of_operations")
     private Integer numberOfOperations;
 
-    public Person(Integer id, String name, String lastname, String email, Integer direction, String password, Integer CVU, Integer wallet) {
+    public User(Integer id, String name, String lastname, String email, Integer direction, String password, Integer CVU, Integer wallet) {
         this.id = id;
         this.name = name;
         this.lastname = lastname;
@@ -38,5 +38,9 @@ public class Person {
         this.password = password;
         this.CVU = CVU;
         this.wallet = wallet;
+    }
+
+    public User() {
+
     }
 }
