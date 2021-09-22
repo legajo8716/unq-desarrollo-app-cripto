1c0a3ed21918a1b07e6cea84e908beebde1a01a7
PabloD97 <pablo97758@gmail.com>
2021-09-22 02:00:31 -0300

    feat: Person Model
    - Se agrego el archivo Person dentro del package model
    - Se agrego la clase Person

---
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/model/Person.java
@@ -0,0 +1,33 @@
+package ar.edu.unq.desapp.grupoD022021.backenddesappapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+    @Column(name = "lastname", length = 30, nullable = false)
+    private String lastname;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "direction", nullable = false, length = 8)
+    private Integer direction;
+    @Column(name = "password", nullable = false, length = 8)
+    private String password;
+    @Column(name = "CVU", nullable = false, length = 22)
+    private Integer CVU;
+    @Column(name = "wallet", nullable = false, length = 8)
+    private Integer wallet;
+
+    @Column(name = "awarded_points")
+    private Integer awardedPoints;
+    @Column(name = "number_of_operations")
+    private Integer numberOfOperations;
+
+
+}
