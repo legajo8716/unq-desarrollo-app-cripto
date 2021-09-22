621aac7f0fb75bb56f48ced66e1d8a181de302ec
PabloD97 <pablo97758@gmail.com>
2021-09-22 20:38:58 -0300

    feat: User Model
    - Se agrego el archivo User en la carpeta model
    - Se agrego la clase User con sus correspondientes atributos

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/model/User.java
@@ -29,5 +29,14 @@ public class Person {
     @Column(name = "number_of_operations")
     private Integer numberOfOperations;
 
-
+    public Person(Integer id, String name, String lastname, String email, Integer direction, String password, Integer CVU, Integer wallet) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.direction = direction;
+        this.password = password;
+        this.CVU = CVU;
+        this.wallet = wallet;
+    }
 }
