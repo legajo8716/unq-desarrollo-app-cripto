0ea0852e070044359183d1ec1f5edbdac836ef73
PabloD97 <pablo97758@gmail.com>
2021-09-23 14:07:09 -0300

    chore: application properties
    - Se agrego la configuracion en el application.properties para h2
    - Se sobrescribio el method configure() de WebSecurity de Springboot para poder loguearse en h2

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/BackendDesappApiApplication.java
@@ -2,9 +2,18 @@ package ar.edu.unq.desapp.grupoD022021.backenddesappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class BackendDesappApiApplication {
+public class BackendDesappApiApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDesappApiApplication.class, args);
