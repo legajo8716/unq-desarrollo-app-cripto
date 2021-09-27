ad54ea88d1df754f80473ec485f10feab617b13c
PabloD97 <pablo97758@gmail.com>
2021-09-27 16:11:08 -0300

    test: CriptoactiveController Test
    - Se agrego el archivo TestConfig
    - Se agrego la clase TestConfig
    - Se agrego el metodo testRestTemplate() para poder hacer los llamados a la api
    - Se agrego el archivo TestWithRestTemplate
    - Se agrego el test del endpoint '/api/cryptoassets'

---
+++ src/test/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/webservice/TestConfig.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.desapp.grupoD022021.backenddesappapi.webservice;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class TestConfig {
+
+    @Bean
+    public TestRestTemplate testRestTemplate(){
+        return new TestRestTemplate();
+    }
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/webservice/TestWithRestTemplate.java
@@ -0,0 +1,54 @@
+package ar.edu.unq.desapp.grupoD022021.backenddesappapi.webservice;
+
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.BackendDesappApiApplication;
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.model.Cryptoactive;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestConfig.class)
+@SpringBootTest(classes = BackendDesappApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestWithRestTemplate {
+
+    @LocalServerPort
+    Integer port;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void testCryptoassets() {
+        ResponseEntity<List> result =  (restTemplate.getForEntity("http://localhost:"+port+"/api/cryptoassets", List.class));
+        assertNotNull(result);
+
+        /**Chequeo que haya traido todos los crypto activos**/
+        assertEquals(14, result.getBody().size());
+
+        /**Chequear que sean solicitados **/
+        List<LinkedHashMap> cryptoassets = (result.getBody());
+        String aliceusdt = (String) cryptoassets.get(0).get("symbol");
+        assertEquals("ALICEUSDT", aliceusdt);
+
+        String MATICUSDT = (String) cryptoassets.get(1).get("symbol");
+        assertEquals("MATICUSDT", MATICUSDT);
+
+        String AUDIOUSDT = (String) cryptoassets.get(13).get("symbol");
+        assertEquals("AUDIOUSDT", AUDIOUSDT);
+
+    }
+}
