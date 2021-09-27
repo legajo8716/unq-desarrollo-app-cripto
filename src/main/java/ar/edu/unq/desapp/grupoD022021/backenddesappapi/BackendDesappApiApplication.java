c000e39e53b879c7fe32bce74402313935e62b46
PabloD97 <pablo97758@gmail.com>
2021-09-27 16:40:14 -0300

    refacto: Limpieza de imports

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/BackendDesappApiApplication.java
@@ -2,18 +2,11 @@ package ar.edu.unq.desapp.grupoD022021.backenddesappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 
 @SpringBootApplication
 public class BackendDesappApiApplication extends WebSecurityConfigurerAdapter {
