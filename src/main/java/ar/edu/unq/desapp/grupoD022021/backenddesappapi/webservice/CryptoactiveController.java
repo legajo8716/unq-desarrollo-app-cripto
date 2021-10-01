8d2149eb363b4024ce410b17a19f789cfdebab19
PabloD97 <pablo97758@gmail.com>
2021-10-01 14:12:12 -0300

    chore: Cors origins
    - Se agrego a cors origin del controller de cryptoactive la url de heroku

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/webservice/CryptoactiveController.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/webservice/CryptoactiveController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://crypto-active-app.herokuapp.com/"})
 public class CryptoactiveController {
 
     @Autowired
