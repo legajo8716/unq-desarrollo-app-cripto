95ea81e68831a427156e73ef88c978e92558e5c5
legajo8716 <nelgonzalez88@gmail.com>
2021-09-30 21:11:35 -0300

    chore: form register

--- src/main/frontend/src/components/Register.jsx
+++ src/main/frontend/src/components/Register.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React,{useState}from "react";
 import {useHistory} from "react-router-dom";
+import {Button, Form} from "react-bootstrap";
 
 const Register = () => {
+    const [datos, setDatos] = useState({
+        name: '',
+        lastName: '',
+        email:'',
+        direction:'',
+        password:'',
+        cvu:'',
+        wallet:''
+    })
+    const handleInputChange = (event) => {
+         console.log(event.target.name)
+         console.log(event.target.value)
+        setDatos({
+            ...datos,
+            [event.target.name] : event.target.value
+        })
+    }
 
-
+    const enviarDatos = () => {
+        console.log('enviando datos...' )
+    }
     return (
         <>
-        <h2>Register</h2>
+
+                <Form onSubmit={enviarDatos}>
+                    <Form.Group className="mb-3"  onChange={handleInputChange} >
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="string" placeholder="Ingrese su nombre" name="name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3"   onChange={handleInputChange}>
+                        <Form.Label>Apellido</Form.Label>
+                        <Form.Control type="string" placeholder="Ingrese su nombre" name="lastName"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3"   onChange={handleInputChange}>
+                    <Form.Label>Email </Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name="email"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3"   onChange={handleInputChange}>
+                        <Form.Label>Direccion</Form.Label>
+                        <Form.Control type="string" placeholder="Ingrese su direccion" name="direction"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3"  onChange={handleInputChange}>
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Ingrese su contraseña"  name="password" />
+                </Form.Group>
+                    <Form.Group className="mb-3"   onChange={handleInputChange}>
+                        <Form.Label>CVU</Form.Label>
+                        <Form.Control type="string" placeholder="Ingrese su CVU" name="cvu"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3"   onChange={handleInputChange}>
+                        <Form.Label>Billetera</Form.Label>
+                        <Form.Control type="string" placeholder="Ingrese su Billetera" name="wallet" />
+                    </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Registrarme
+                </Button>
+            </Form>
         </>
     )
 }
