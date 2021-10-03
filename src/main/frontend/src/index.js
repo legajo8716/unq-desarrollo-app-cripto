bd001877e2dfe57dc0e085b2ddb7ca3f59ce6442
legajo8716 <nelgonzalez88@gmail.com>
2021-10-03 19:19:14 -0300

    merge

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import axios from 'axios'
-axios.defaults.headers['Authorization']='nelsiton'
 ReactDOM.render(
     <App />,
   document.getElementById('root')
