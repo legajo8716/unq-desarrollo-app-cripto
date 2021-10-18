183ce9e0da36e4044bfe42ca986deada0764eefb
PabloD97 <pablo97758@gmail.com>
2021-10-18 19:26:04 -0300

    feat: Caching Cryptoassets
    - Se agrego el cacheo para la consulta de los crypto-activos

---
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/config/CachingConfig.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.desapp.grupoD022021.backenddesappapi.config;
+
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("cryptoassets");
+    }
+}
