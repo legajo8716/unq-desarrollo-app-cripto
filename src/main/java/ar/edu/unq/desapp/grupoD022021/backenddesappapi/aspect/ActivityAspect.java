3d701978943eeb3b1f72ed54da53e08add55da8a
Nelson <nelgonzalez88@gmail.com>
2021-11-21 21:52:20 -0300

    agrego metodos en aspectos

--- src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/aspect/ActivityAspect.java
+++ src/main/java/ar/edu/unq/desapp/grupoD022021/backenddesappapi/aspect/ActivityAspect.java
@@ -4,6 +4,10 @@ package ar.edu.unq.desapp.grupoD022021.backenddesappapi.aspect;
 
 import ar.edu.unq.desapp.grupoD022021.backenddesappapi.dto.TransactionDTO;
 import ar.edu.unq.desapp.grupoD022021.backenddesappapi.model.Transaction;
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.model.User;
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.repositories.UserRepository;
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.security.jwt.JwtRequest;
+import ar.edu.unq.desapp.grupoD022021.backenddesappapi.security.jwt.JwtResponse;
 import io.netty.util.internal.logging.Log4J2LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
@@ -13,11 +17,13 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 import java.sql.SQLSyntaxErrorException;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -25,15 +31,30 @@ import java.util.List;
 @Aspect
 @Component
 public class ActivityAspect {
-    @Around(value = "execution(* ar.edu.unq.desapp.grupoD022021.backenddesappapi.service.TransactionService.getAllTransaction())")
+    @Autowired
+        UserRepository userRepository;
+
+    @Around(value = "execution(* ar.edu.unq.desapp.grupoD022021.backenddesappapi.service.*.*(..))")
 public void getServicesExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
         proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
         long endTime = System.currentTimeMillis() - startTime;
 
+
+        if (proceedingJoinPoint.getArgs().length<0){
+            log.info("El metodo no tiene parametros");
+        }
+        else{
+            int nroParametro=0;
+            for  (Object param : proceedingJoinPoint.getArgs()){
+                ;
+                log.info("Parametro"+ nroParametro+param.toString());
+            }
+        }
         log.info("Nombre del metodo:" +proceedingJoinPoint.getSignature().getName());
 
         log.info("Tiempo de ejecucion: "+endTime+" ms");
+
 
 
 
